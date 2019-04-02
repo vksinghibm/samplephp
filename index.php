28284674740fc1d0c6949dc3f4b20f5853a0f988
VINOD K. SINGH <vksingh@us.ibm.com>
2019-04-02 16:52:53 -0500

    updated the code

--- index.php
+++ index.php
@@ -1,3 +1,3 @@
 <?php
-    echo "This is the sample application on OpenShift from host  ".gethostname()
+    echo "This is the "latest code changes" in applicationn on OpenShift from host  ".gethostname()
 ?>
